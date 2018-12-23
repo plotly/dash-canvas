43ff2ff02cd84cbb11a26086f394efdcedfc8c6c
Emmanuelle Gouillart <emmanuelle.gouillart@nsup.org>
2018-12-23 23:03:15 +0100

    Removed sketchfielddemo

--- setup.py
+++ setup.py
@@ -8,13 +8,15 @@ with open(os.path.join('dash_canvas', 'package.json')) as f:
 
 package_name = package["name"].replace(" ", "_").replace("-", "_")
 
+
 setup(
     name=package_name,
     version=package["version"],
     author=package['author'],
+    url='https://github.com/plotly/dash-canvas',
     packages=[package_name],
     include_package_data=True,
     license=package['license'],
     description=package['description'] if 'description' in package else package_name,
-    install_requires=[]
+    install_requires=['dash', 'dash-html-components']
 )
