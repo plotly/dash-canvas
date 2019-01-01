60d0e1a5d954c8c28d1b17075c0f28fe472fc161
Emmanuelle Gouillart <emmanuelle.gouillart@nsup.org>
2019-01-01 22:20:06 +0100

    updated integration test

--- tests/test_usage.py
+++ tests/test_usage.py
@@ -1,10 +1,25 @@
-# Basic test for the component rendering.
-def test_render_component(dash_app, selenium):
-    # Start a dash app contained in `usage.py`
-    # dash_app is a fixture by pytest-dash
-    # It will load a py file containing a Dash instance named `app`
-    # and start it in a thread.
-    app = dash_app('correct_segmentation.py')
+import shutil
+import sys
 
-    # Get the generated component with selenium
-    # my_component = selenium.find_element_by_id('input')
+
+from pytest_dash.utils import (
+    import_app,
+    wait_for_text_to_equal,
+    wait_for_element_by_css_selector
+)
+
+
+def test_install(cookies, dash_threaded, selenium):
+    results = cookies.bake(extra_context={
+        'project_name': 'DashCanvas',
+        'author_name': 'test',
+        'author_email': 'test',
+    })
+
+    # Add the generated project to the path so it can be loaded from usage.py
+    # It lies somewhere in a temp directory created by pytest-cookies
+    sys.path.insert(0, str(results.project))
+
+    # Test that `usage.py` works after building the default component.
+    dash_threaded(import_app('correct_segmentation'))
+
