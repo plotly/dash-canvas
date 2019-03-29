4c01d443e33763fa3912efd246bd95c15421cc15
Emmanuelle Gouillart <emmanuelle.gouillart@nsup.org>
2019-03-29 08:01:38 +0100

    fixed dash tests

--- app.py
+++ app.py
@@ -1,145 +1,60 @@
-import numpy as np
-import json
-from skimage import io
-from PIL import Image
 
-
-import dash_canvas
+from dash_canvas import DashCanvas
 import dash
 from dash.dependencies import Input, Output, State
 import dash_html_components as html
 import dash_core_components as dcc
 import plotly.graph_objs as go
-from dash.exceptions import PreventUpdate
-
-from dash_canvas.utils.parse_json import parse_jsonstring
-from dash_canvas.utils.image_processing_utils import segmentation_generic
-from dash_canvas.utils.plot_utils import image_with_contour
-from dash_canvas.utils.io_utils import image_string_to_PILImage
-
-from dash_canvas.components import image_upload_zone
-
-# Image to segment and shape parameters
-filename = 'https://upload.wikimedia.org/wikipedia/commons/e/e4/Mitochondria%2C_mammalian_lung_-_TEM_%282%29.jpg'
-img = io.imread(filename, as_gray=True)
-height, width = img.shape
-canvas_width = 500
-canvas_height = round(height * canvas_width / width)
-scale = canvas_width / width
+import dash_daq as daq
 
 
-# ------------------ App definition ---------------------
+filename = 'https://www.publicdomainpictures.net/pictures/60000/nahled/flower-outline-coloring-page.jpg'
+canvas_width = 300
 
 app = dash.Dash(__name__)
-server = app.server
-app.config.suppress_callback_exceptions = True
 
 app.layout = html.Div([
-    html.Div([
-        html.H2(children='Segmentation tool'),
-        dcc.Markdown('''
-            Draw on the picture to annotate each object
-            you want to segment, then press the "Segmentation"
-            button to trigger the segmentation.
-        '''),
-        dash_canvas.DashCanvas(
-            id='canvas',
-            width=canvas_width,
-            height=canvas_height,
-            scale=scale,
-            filename=filename,
-            goButtonTitle='Segmentation'
+        html.Div([
+            DashCanvas(
+                id='canvas-color',
+                width=canvas_width,
+                filename=filename,
+                hide_buttons=['line', 'zoom', 'pan'],
+                )
+                ], className="six columns"),
+        html.Div([
+            html.H6(children=['Brush width']),
+            dcc.Slider(
+                id='bg-width-slider',
+                min=2,
+                max=40,
+                step=1,
+                value=[5]
+            ),
+            daq.ColorPicker(
+                id='color-picker',
+                label='Brush color',
+                value='#119DFF'
         ),
-        image_upload_zone('upload-image'),
-        dcc.Dropdown(
-            id='algorithm',
-            options=[
-                {'label': 'Watershed', 'value': 'watershed'},
-                {'label': 'Random Walker', 'value': 'random_walker'},
-                {'label': 'Random Forest', 'value': 'random_forest'}
-            ],
-            value='watershed'
-        ),
-        ], className="six columns"),
-    html.Div([
-        html.H2(children='Segmentation result'),
-        dcc.Graph(
-            id='segmentation',
-            figure=image_with_contour(np.ones_like(img),
-                                      img > 0, shape=(height, width))
-        )
-        ], className="six columns")],# Div
-    className="row")
-
-# ----------------------- Callbacks -----------------------------
+        ], className="three columns"),
+        ])
 
 
-@app.callback(Output('segmentation', 'figure'),
-            [Input('canvas', 'image_content'),
-            Input('canvas', 'json_data'),
-            Input('canvas', 'height')],
-            [State('canvas', 'scale'),
-            State('canvas', 'width'),
-            State('algorithm', 'value')])
-def update_figure_upload(image, string, h, s, w, algorithm):
-    mask = parse_jsonstring(string, shape=(round(h/s), round(w/s)))
-    if mask.sum() > 0:
-        if image is None:
-            im = img
-            image = img
-        else:
-            im = image_string_to_PILImage(image)
-            im = np.asarray(im)
-        seg = segmentation_generic(im, mask, mode=algorithm)
+@app.callback(Output('canvas-color', 'lineColor'),
+            [Input('color-picker', 'value')])
+def update_canvas_linewidth(value):
+    if isinstance(value, dict):
+        return value['hex']
     else:
-        if image is None:
-            image = img
-        seg = np.zeros((h, w))
-    return image_with_contour(image, seg, shape=(round(h/s), round(w/s)))
+        return value
 
 
-
-@app.callback(Output('canvas', 'image_content'),
-            [Input('upload-image', 'contents')])
-def update_canvas_upload(image_string):
-    if image_string is None:
-        raise PreventUpdate
-    if image_string is not None:
-        return image_string
-    else:
-        return None
-
-
-@app.callback(Output('canvas', 'height'),
-            [Input('upload-image', 'contents')],
-            [State('canvas', 'width'),
-            State('canvas', 'height')])
-def update_canvas_upload_shape(image_string, w, h):
-    if image_string is None:
-        raise PreventUpdate
-    if image_string is not None:
-        # very dirty hack, this should be made more robust using regexp
-        im = image_string_to_PILImage(image_string)
-        im_h, im_w = im.height, im.width
-        return round(w / im_w * im_h)
-    else:
-        return canvas_height
-
-
-@app.callback(Output('canvas', 'scale'),
-            [Input('upload-image', 'contents')])
-def update_canvas_upload_scale(image_string):
-    if image_string is None:
-        raise PreventUpdate
-    if image_string is not None:
-        # very dirty hack, this should be made more robust using regexp
-        im = image_string_to_PILImage(image_string)
-        im_h, im_w = im.height, im.width
-        return canvas_width / im_w
-    else:
-        return scale
-
+@app.callback(Output('canvas-color', 'lineWidth'),
+            [Input('bg-width-slider', 'value')])
+def update_canvas_linewidth(value):
+    return value
 
 if __name__ == '__main__':
     app.run_server(debug=True)
+
 
--- tests/test_usage.py
+++ tests/test_usage.py
@@ -8,16 +8,11 @@ from pytest_dash.wait_for import (
 )
 from pytest_dash.application_runners import import_app
 
-def test_install(cookies, dash_threaded, selenium):
-    results = cookies.bake(extra_context={
-        'project_name': 'DashCanvas',
-        'author_name': 'test',
-        'author_email': 'test',
-    })
+def test_install(dash_threaded):
 
     # Add the generated project to the path so it can be loaded from usage.py
     # It lies somewhere in a temp directory created by pytest-cookies
-    sys.path.insert(0, str(results.project))
+    sys.path.insert(0, '..')
 
     # Test that `usage.py` works after building the default component.
     dash_threaded(import_app('app'))
