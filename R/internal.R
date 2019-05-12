ec63a0585b25a2735294a61392ca5698660d8832
Ryan Patrick Kyle <ryan@plot.ly>
2019-05-11 20:22:29 -0400

    :package: initial R package commit

---
+++ R/internal.R
@@ -0,0 +1,16 @@
+.dashCanvas_js_metadata <- function() {
+deps_metadata <- list(`dash_canvas` = structure(list(name = "dash_canvas",
+version = "0.0.10", src = list(href = NULL,
+file = "deps"), meta = NULL,
+script = "dash_canvas.min.js",
+stylesheet = NULL, head = NULL, attachment = NULL, package = "dashCanvas",
+all_files = FALSE), class = "html_dependency"),
+`dash_canvas_dev` = structure(list(name = "dash_canvas",
+version = "0.0.10", src = list(href = NULL,
+file = "deps"), meta = NULL,
+script = "dash_canvas.dev.js",
+stylesheet = NULL, head = NULL, attachment = NULL, package = "dashCanvas",
+all_files = FALSE), class = "html_dependency")
+)
+return(deps_metadata)
+}
