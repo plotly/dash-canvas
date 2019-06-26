01c92338351fbb277397a59375be0bd58585b378
Ryan Patrick Kyle <ryan@plot.ly>
2019-06-26 17:09:57 -0400

    :package: updated R pkg for dash 0.1.0

--- R/internal.R
+++ R/internal.R
@@ -2,15 +2,25 @@
 deps_metadata <- list(`dash_canvas` = structure(list(name = "dash_canvas",
 version = "0.0.10", src = list(href = NULL,
 file = "deps"), meta = NULL,
-script = "dash_canvas.min.js",
+script = 'dash_canvas.min.js',
 stylesheet = NULL, head = NULL, attachment = NULL, package = "dashCanvas",
-all_files = FALSE), class = "html_dependency"),
-`dash_canvas_dev` = structure(list(name = "dash_canvas",
-version = "0.0.10", src = list(href = NULL,
-file = "deps"), meta = NULL,
-script = "dash_canvas.dev.js",
-stylesheet = NULL, head = NULL, attachment = NULL, package = "dashCanvas",
-all_files = FALSE), class = "html_dependency")
-)
+all_files = FALSE), class = "html_dependency"))
 return(deps_metadata)
+}
+
+dash_assert_valid_wildcards <- function (attrib = list("data", "aria"), ...)
+{
+    args <- list(...)
+    validation_results <- lapply(names(args), function(x) {
+        grepl(paste0("^", attrib, "-[a-zA-Z0-9]{1,}$", collapse = "|"),
+            x)
+    })
+    if (FALSE %in% validation_results) {
+        stop(sprintf("The following wildcards are not currently valid in Dash: '%s'",
+            paste(names(args)[grepl(FALSE, unlist(validation_results))],
+                collapse = ", ")), call. = FALSE)
+    }
+    else {
+        return(args)
+    }
 }
