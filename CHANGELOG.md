c964d949733b5fffef8cd1ea0c23acfabbf4cc59
Marc-André Rivet <Marc-Andre-Rivet@users.noreply.github.com>
2020-02-27 14:25:52 -0500

    Issue 43 - Rename async modules (#44)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 All notable changes to this project will be documented in this file.
 This project adheres to [Semantic Versioning](http://semver.org/).
 
+## [Unreleased]
+### Changed
+- [#44](https://github.com/plotly/dash-canvas/pull/44) Renamed async modules with hyphen `-` instead of tilde `~`
+
 ## [0.1.0] - 2019-11-05
 ### Added
 - [#29](https://github.com/plotly/dash-canvas/pull/29) Async canvas component
--- tests/test_dash_canvas.py
+++ tests/test_dash_canvas.py
@@ -1,16 +1,19 @@
 import json
+from functools import partial
 
 from skimage import img_as_ubyte
 import numpy as np
 
 import dash
 from dash.dependencies import Input, Output, State
+from dash.exceptions import PreventUpdate
 import dash_html_components as html
 import dash_core_components as dcc
 
 import dash_canvas
 from dash_canvas.utils import array_to_data_url
 
+from selenium.webdriver.support.ui import WebDriverWait
 
 def _get_button_by_title(dash_duo, title):
     return dash_duo.wait_for_element(
@@ -18,10 +21,15 @@ def _get_button_by_title(dash_duo, title):
     )
 
 
+TIMEOUT = 10
+
 def test_canvas_undo_redo(dash_duo):
     h, w = 10, 10
     overlay = np.zeros((h, w), dtype=np.uint8)
     overlay = img_as_ubyte(overlay)
+
+    calls = 0
+    data_saved = []
 
     # Set up a small app. This could probably be made into a fixture.
     app = dash.Dash(__name__)
@@ -36,15 +44,23 @@ def test_canvas_undo_redo(dash_duo):
         )
     ])
 
-    data_saved = []
-
     @app.callback(
         Output('cache', 'data'),
         [Input("canvas", "trigger")],
         [State("canvas", "json_data")]
     )
     def update_overlay(flag, data):
+        if flag is None or data is None:
+            raise PreventUpdate
+
         data_saved.append(data)
+
+        nonlocal calls
+        calls = calls + 1
+
+    def calls_equals(count, driver):
+        nonlocal calls
+        return calls == count
 
     dash_duo.start_server(app)
 
@@ -54,6 +70,7 @@ def test_canvas_undo_redo(dash_duo):
     btn = _get_button_by_title(dash_duo, "Save")
     btn.click()
 
+    WebDriverWait(dash_duo.driver, TIMEOUT).until(partial(calls_equals, 1))
     objs_1 = json.loads(data_saved[-1])['objects']
     assert len(objs_1) > 0
 
@@ -63,6 +80,7 @@ def test_canvas_undo_redo(dash_duo):
     btn.click()
     btn = _get_button_by_title(dash_duo, "Save")
     btn.click()
+    WebDriverWait(dash_duo.driver, TIMEOUT).until(partial(calls_equals, 2))
 
     objs_2 = json.loads(data_saved[-1])['objects']
     assert objs_2 == []
@@ -72,6 +90,7 @@ def test_canvas_undo_redo(dash_duo):
     btn.click()
     btn = _get_button_by_title(dash_duo, "Save")
     btn.click()
+    WebDriverWait(dash_duo.driver, TIMEOUT).until(partial(calls_equals, 3))
 
     objs_3 = json.loads(data_saved[-1])['objects']
     assert objs_1 == objs_3
